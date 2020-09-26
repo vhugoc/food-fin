29c0153625143ca9b4bc94d6f912c9524ae1ffff
Victor Hugo <vhuugoc@gmail.com>
2020-09-26 19:41:48 -0300

    Configuração dos middlewares iniciais

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 // Require credentials
 const credentials = require('./configs/database/credentials.json');
 // Require connection class
@@ -14,9 +15,16 @@ class Application {
   // Run the server
   start() {
     this.connection.connect();
+    this.middlewares();
     this.express.listen(process.env.PORT || this.port, () => {
       console.log(`Listening on port ${this.port}`);
     });
+  }
+
+  // Middlewares initialization
+  middlewares() {
+    this.express.use(express.json());
+    this.express.use(morgan('tiny'));
   }
 }
 
