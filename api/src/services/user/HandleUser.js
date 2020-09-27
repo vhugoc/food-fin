0effae6f3bfb2e9b6b09a4af6ef6883a2c49b16b
Victor Hugo <vhuugoc@gmail.com>
2020-09-27 16:55:58 -0300

    Features para excluir um usuário

--- api/src/services/user/HandleUser.js
+++ api/src/services/user/HandleUser.js
@@ -7,10 +7,40 @@ class HandleUser {
   /**
    * Checks if an user exists
    * @param { string } email 
+   * @param { string } id user id
    */
-  async exists(email) {
+  async exists(email = null, id = null) {
     try {
-      const exists = await User.findOne({ email: email });
+      let exists;
+
+      if (!id) {
+        exists = await User.findOne({ email: email });
+      } else {
+        exists = await User.findOne({
+          $and: [
+            { email: { $eq: req.user_id } },
+            { _id: { $ne: id } }
+          ]
+        });
+      }
+
+      if (exists)
+        return true;
+
+      return false;
+
+    } catch(error) {
+      return { error: true, description: error, status: 500 };
+    }
+  }
+
+  /**
+   * Check if an user exists by id
+   * @param { string } id 
+   */
+  async exists_by_id(id) {
+    try {
+      const exists = await User.findById(id);
 
       if (exists)
         return true;
@@ -35,11 +65,11 @@ class HandleUser {
     try {
       // Email validation
       if (!await MainHelper.validate_email(email))
-        return { success: false, description: "Invalid email", status: 400 }
+        return { success: false, description: "Invalid email", status: 400 };
 
       // Check if email exists
       if (await this.exists(email))
-        return { success: false, description: "User already exists", status: 400 }
+        return { success: false, description: "User already exists", status: 400 };
 
       // Password encrypt
       const pwd = await MainHelper.encrypt(password);
@@ -55,7 +85,28 @@ class HandleUser {
       });
 
       if (!create)
-        return { success: false, description: "Internal Error", status: 500 }
+        return { success: false, description: "Internal Error", status: 500 };
+
+      return { success: true, status: 200 };
+
+    } catch(error) {
+      return { error: true, description: error, status: 500 };
+    }
+  }
+
+  /**
+   * Delete an user by id
+   * @param { string } id 
+   */
+  async delete(id) {
+    try {
+      if (!this.exists_by_id(id))
+        return { success: false, description: "User does not exists", status: 400 };
+
+      const del = await User.deleteOne({ _id: id });
+
+      if (!del)
+        return { success: false, description: "Internal Error", status: 500 };
 
       return { success: true, status: 200 };
 
